9d7d553b3811276779699848c8e1cda7bb3e744b
Michael Pérez-Ortiz <perezortizm7@gmail.com>
2025-07-15 14:33:49 -0400

    Created map routes

--- backend/index.js
+++ backend/index.js
@@ -100,6 +100,96 @@ try {
 
 
 
+//Map Routes
+
+//GET
+app.get("api/maps" , async(req ,res) => {
+try {
+
+    const maps = await Map.find().sort({chapter: 1})
+    res.json(maps)
+} catch(error) {
+    res.status(500).json({message: error.message})
+}
+
+});
+
+
+app.get("api/maps/:chapter" , async(req ,res) => {
+try {
+
+    const maps = await Map.findOne({chapter: req.params.chapter})
+    .populate("enemyUnits")
+    .populate("allyUnits")
+    
+    if(!map) {
+        return res.status(404).json({message: "Map Not Found"})
+    }
+
+    res.json(map);
+} catch(error) {
+    res.status(500).json({message: error.message})
+}
+
+});
+
+//POST
+
+app.post("/api/maps" , async(req , res) => {
+
+    const map = new Map({
+         name: req.body.name ,
+         description: req.body.description ,
+         grid: req.body.grid ,
+         chapter: req.body.chapter ,
+         enemyUnits: req.body.enemyUnits ,
+         allyUnits: req.body.allyUnits
+    });
+try {
+
+    const newMap = await Map.save();
+    res.status(201).json(newMap);
+} catch(error) {
+    res.status(400).json({message: error.message})
+}
+
+});
+
+//UPDATE
+
+app.put("/api/maps/:id" , async(req ,res) => {
+
+try {
+
+    const updateMap = await Map.findByIdAndUpdate(req.params.id , req.body , {new:true});
+    res,json(updateMap);
+
+
+    } catch(error) {
+        res.status(400).json({message: error.message});
+    }
+});
+
+//DELETE
+
+app.delete("/api/maps/:id" , async(req , res) => {
+try {
+
+    await Map.findByIdAndDelete(req.params.id);
+    res.json({message: "Map Deleted"});
+
+    } catch(error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+
+//Dialogue Routes
+
+
+
+
+
 
 
 
