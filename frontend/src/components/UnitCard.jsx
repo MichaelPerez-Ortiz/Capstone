4e9756b7af5f281faf9913d5f3e6cdf26d7bd1ab
Michael Pérez-Ortiz <perezortizm7@gmail.com>
2025-07-17 11:49:41 -0400

    Completed unit card component

---
+++ frontend/src/components/UnitCard.jsx
@@ -0,0 +1,57 @@
+import UnitPortrait from "./UnitPortrait";
+
+function UnitCard({unit , isSelected , onClick}) {
+    return (
+        <div className = {`unitCard ${isSelected ? "selected" : ""}`}
+        onClick = {() => onClick(unit)}>
+
+
+
+            <div className = "cardPortrait">
+                <UnitPortrait unit = {unit} size = "medium"/>
+            </div>
+
+        <div className = "cardInfo">
+            <h3> {unit.name} </h3>
+            <p className = "unitClass"> {unit.class} </p>
+
+            <div className = "cardStats">
+
+                <div className = "stat">
+                    <span className = "statLabel"> HP </span>
+                    <span className = "statValue"> {unit.stats.hp} </span>
+                </div>
+           
+                <div className = "stat">
+                    <span className = "statLabel"> ATK </span>
+                    <span className = "statValue"> {unit.stats.atk} </span>
+                </div>
+          
+                <div className = "stat">
+                    <span className = "statLabel"> DEF </span>
+                    <span className = "statValue"> {unit.stats.def} </span>
+                </div>
+          
+                <div className = "stat">
+                    <span className = "statLabel"> SPD </span>
+                    <span className = "statValue"> {unit.stats.spd} </span>
+                </div>
+            
+                <div className = "stat">
+                    <span className = "statLabel"> MOV </span>
+                    <span className = "statValue"> {unit.stats.mov} </span>
+                </div>
+            </div>
+        </div>
+
+        {isSelected && (
+            <div className = "unitSelected">
+                <span></span>
+            </div>
+        )}
+
+     </div>
+    );
+}
+
+export default UnitCard
